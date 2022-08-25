8fd13c819fa600d74fefa6c9aa622e4dc9014712
Lion Wallin <lion.wallin@ga.ntig.se>
2022-08-25 09:13:31 +0200

    first commit

---
+++ lektion.py
@@ -0,0 +1,6 @@
+år = int(input("Hur många år är du?"))
+månader = int(input("Hur många månader är du?"))
+
+sekunderår = år*31536000
+sekundermånader = månader*2629800
+print(f"Du sa att du var {år} gammal och {månader} gammal det gör att du är {sekunderår + sekundermånader} sekunder gammal")
